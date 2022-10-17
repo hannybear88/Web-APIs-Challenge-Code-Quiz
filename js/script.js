f8b0234fc89e0a29de3c7efe522f418067057cda
hannybear88 <hannahkchung88@gmail.com>
2022-10-16 20:29:25 -0700

    fixed issue with icons

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Gathering all rquired elements
+const begin_btn = document.querySelector(".begin_btn button");
+const quiz_rules_box = document.querySelector(".quiz_rules_box button");
+const quit_btn = document.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
+
