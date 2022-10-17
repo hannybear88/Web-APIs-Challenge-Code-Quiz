48413c7d993f6878d9da729211d84f995f8df1a4
hannybear88 <hannahkchung88@gmail.com>
2022-10-16 21:59:02 -0700

    added js.questions file

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+
+let questions = [
+    {
+    number: 1,
+    question: "Arrays in JavaScript can be used to store ________.",
+    answer: "All of the Above",
+    options: [
+        A. "Numbers and Strings",
+        B. "Other Arrays",
+        C. "Booleans",
+        D. "All of the Above",
+    ]
+},
+    {
+    number: 2, 
+    question: "String Values must be enclosed with _______ when being assigned to variables.",
+    answer: "Quotes",
+    options: [
+        A. "Commas",
+        B. "Curly Brackets",
+        C. "Quotes", 
+        D. "Parenthesis",
+    ]
+},
+    {
+    number: 3, 
+    question: "Commonly used data types DO NOT include:",
+    answer: "Alerts",
+    options: [
+        A."Strings",
+        B. "Booleans",
+        C. "Alerts",
+        D. "Numbers",
+    ]
+},
+    {
+    number: 4, 
+    question: "A very useful tool use during development and debugging for printing content to the debugger is:",
+    answer: "Console.Log",
+    options: [
+        A. "JavaScript",
+        B. "Terminal/Bash",
+        C. "For Loops",
+        D. "Console.Log",
+
+},
+    {
+    number: 5, 
+    question: "The condition in an if / else statement is enclosed with ___________. ",
+    answer: "Parenthesis",
+    options: [
+        A. "Quotes",
+        B. "Curly Brackets",
+        C. "Parenthesis",
+        D. "Square Brackets",
+        
+},
+    {
+    number: 6, 
+    question: "Which of the following two CSS properties are used to set an HTML element’s foreground color and background color, respectively?",
+    answer: "Color and Background-Color",
+    options: [
+        A. "Foreground-Color and Background-Color",
+        B. "Color and Background-Color",
+        C. "Color-Foreground and Color-Background",
+        D. "Color-Font and Color-Back",
+    ]
+},
+    {
+    number: 7, 
+    question: What is the correct definition of an HTML element?",
+    answer: "An HTML tag and the content that it contains or marks up",
+    options: [
+        A. "The first, or opening, HTML tag",
+        B. "HTML code that does not requir opening or closing tags",
+        C. "An HTML tag adn the content that it contains or marks up",
+        D. "The second, or closing, HTML tag. Closing tags have a forward slash (/) inside of them.",
+    ]
+},
+    {
+    number: 8, 
+    question: " What is a directory?",
+    answer: "A folder used to store files.",
+    options: [
+        A. "A folder used to store files.",
+        B. "A Tree Structure",
+        C. "A command to a computer",
+        D. "A file",
+    ]
+},
+    {
+    number: 9, 
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+        A. "Application Processes Integration",
+        B. "Application Programming Interface",
+        C. "Application Platform Interface",
+        D. "Application Programmer Interface",
+    ]
+},
+    {
+    number: 10, 
+    question: "S What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+        A. "Computer Systems Support",
+        B. "Custom Style Sheet",
+        C. "Computer SoftwareServices",
+        D. "Cascading Style Sheet",
+    ]
+},
+
--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 //Gathering all rquired elements
 const begin_btn = document.querySelector(".begin_btn button");
 const quiz_rules_box = document.querySelector(".quiz_rules_box button");
-const quit_btn = document.querySelector(".buttons .quit");
+const leave_btn = quiz_rules_box.querySelector(".buttons .quit");
 const continue_btn = document.querySelector(".buttons .restart");
 
+//If User Clicks Begin Quiz Button
+begin_btn.onclick = ()=>{
+    quiz_rules_box.classList.add("activeInfo"); // Infi Box is Shown
+}
+
+//If User Clicks Leave Button
+leave_btn.onclick = ()=>{
+    quiz_rules_box.classList.remove("activeInfo"); //Info Box is Hidden
+}
