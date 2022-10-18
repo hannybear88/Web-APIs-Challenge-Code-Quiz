f32649b87267595328e398ab5a4a23d60d4d2303
hannybear88 <hannahkchung88@gmail.com>
2022-10-18 16:44:33 -0700

    added script.js code and redid code for questions.js code

--- js/questions.js
+++ js/questions.js
@@ -1,113 +1,114 @@
-
-let questions = [
+var myQuestions = [
     {
     number: 1,
     question: "Arrays in JavaScript can be used to store ________.",
-    answer: "All of the Above",
-    options: [
-        A. "Numbers and Strings",
-        B. "Other Arrays",
-        C. "Booleans",
-        D. "All of the Above",
-    ]
+    choices: {
+        A: 'Numbers and Strings',
+        B: 'Other Arrays',
+        C: 'Booleans',
+        D: 'All of the Above',
+    },
+    correctAnswer: 'D'
 },
     {
     number: 2, 
     question: "String Values must be enclosed with _______ when being assigned to variables.",
-    answer: "Quotes",
-    options: [
-        A. "Commas",
-        B. "Curly Brackets",
-        C. "Quotes", 
-        D. "Parenthesis",
-    ]
+    choices: {
+        A: 'Commas',
+        B: 'Curly Brackets',
+        C: 'Quotes', 
+        D: 'Parenthesis',
+    },
+    correctAnswer: 'Quotes',
 },
     {
     number: 3, 
     question: "Commonly used data types DO NOT include:",
-    answer: "Alerts",
-    options: [
-        A."Strings",
-        B. "Booleans",
-        C. "Alerts",
-        D. "Numbers",
-    ]
+    choices: {
+        A:'Strings',
+        B: 'Booleans',
+        C: 'Alerts',
+        D: 'Numbers',
+    },
+    correctAnswer: 'Alerts',
 },
     {
     number: 4, 
     question: "A very useful tool use during development and debugging for printing content to the debugger is:",
-    answer: "Console.Log",
-    options: [
-        A. "JavaScript",
-        B. "Terminal/Bash",
-        C. "For Loops",
-        D. "Console.Log",
-
-},
+    choices: {
+        A: 'JavaScript',
+        B: 'Terminal/Bash',
+        C: 'For Loops',
+        D: 'Console.Log',
+    },
+    correctAnswer: 'Console.Log',
+ },
     {
     number: 5, 
     question: "The condition in an if / else statement is enclosed with ___________. ",
-    answer: "Parenthesis",
-    options: [
-        A. "Quotes",
-        B. "Curly Brackets",
-        C. "Parenthesis",
-        D. "Square Brackets",
-        
+    choices: {
+        A: 'Quotes',
+        B: 'Curly Brackets',
+        C: 'Parenthesis',
+        D: 'Square Brackets',
+            },
+    correctAnswer: 'Parenthesis',
 },
+
     {
     number: 6, 
     question: "Which of the following two CSS properties are used to set an HTML element’s foreground color and background color, respectively?",
-    answer: "Color and Background-Color",
-    options: [
-        A. "Foreground-Color and Background-Color",
-        B. "Color and Background-Color",
-        C. "Color-Foreground and Color-Background",
-        D. "Color-Font and Color-Back",
-    ]
+    choices: {
+        A: 'Foreground-Color and Background-Color',
+        B: 'Color and Background-Color',
+        C: 'Color-Foreground and Color-Background',
+        D: 'Color-Font and Color-Back',
+    correctAnswer: 'Color and Background-Color',
+    },
 },
     {
     number: 7, 
-    question: What is the correct definition of an HTML element?",
-    answer: "An HTML tag and the content that it contains or marks up",
-    options: [
-        A. "The first, or opening, HTML tag",
-        B. "HTML code that does not requir opening or closing tags",
-        C. "An HTML tag adn the content that it contains or marks up",
-        D. "The second, or closing, HTML tag. Closing tags have a forward slash (/) inside of them.",
-    ]
+    question: "What is the correct definition of an HTML element?",
+   choices: {
+        A: 'The first, or opening, HTML tag',
+        B: 'HTML code that does not requir opening or closing tags',
+        C: 'An HTML tag adn the content that it contains or marks up',
+        D: 'The second, or closing, HTML tag. Closing tags have a forward slash (/) inside of them.',
+   },
+    correctAnswer: 'An HTML tag and the content that it contains or marks up',
 },
     {
     number: 8, 
-    question: " What is a directory?",
-    answer: "A folder used to store files.",
-    options: [
-        A. "A folder used to store files.",
-        B. "A Tree Structure",
-        C. "A command to a computer",
-        D. "A file",
-    ]
+    question: "What is a directory?",
+    choices: {
+        A: 'A folder used to store files.',
+        B: 'A Tree Structure',
+        C: 'A command to a computer',
+        D: 'A file',
+    },
+    correctAnswer: 'A folder used to store files.',
+    
 },
     {
     number: 9, 
     question: "What does API stand for?",
-    answer: "Application Programming Interface",
-    options: [
-        A. "Application Processes Integration",
-        B. "Application Programming Interface",
-        C. "Application Platform Interface",
-        D. "Application Programmer Interface",
-    ]
+    choices: {
+        A: 'Application Processes Integration',
+        B: 'Application Programming Interface',
+        C: 'Application Platform Interface',
+        D: 'Application Programmer Interface',
+    },
+    correctAnswer: 'Application Programming Interface',
 },
     {
     number: 10, 
     question: "S What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-        A. "Computer Systems Support",
-        B. "Custom Style Sheet",
-        C. "Computer SoftwareServices",
-        D. "Cascading Style Sheet",
-    ]
-},
-
+    choices: {
+        A: 'Computer Systems Support',
+        B: 'Custom Style Sheet',
+        C: 'Computer SoftwareServices',
+        D: 'Cascading Style Sheet',
+    },
+    correctAnswer: 'Cascading Style Sheet',
+    }
+];
